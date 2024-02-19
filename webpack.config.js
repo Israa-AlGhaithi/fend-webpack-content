bf408c635a556753f904b48250a7dacc0147704e
Israa Al-Ghaithi <issralghaithi@hotmail.com>
2024-02-19 07:45:25 +0400

    Line 1 in formHandler.js adding 'export'. Line 1 in nameChecker.js adding 'export'. adding the file .babelrc with the content. Lines 16 & 17 in package.json install the latest webpack and webpack-cli with the command >npm i webpack@latest >npm i webpack-cli@latest Lines 19-22 in package.json install the babel. Line 6 in ./src/client/views/index.html adding the link of distribution main.js and delete all the previous assets Lines 1,2 and 4 in ./src/client/index.js adding the import of the js assets files and logged to console to check. Line 7 in webpack.config.js file in order to fixed the error. Lines 8-16 in webpack.config.js add the new babel loader.

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,16 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-    entry: './src/client/index.js'
+    entry: './src/client/index.js',
+   // output: { ...output options },
+   mode: 'development',
+    module: {
+        rules: [
+                {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: "babel-loader"
+                }
+        ]
+}
 }

1921da3709bddb49e124ec9db361d0edda6ee76c
Israa Al-Ghaithi <issralghaithi@hotmail.com>
2024-02-26 07:24:05 +0400

    These modification for the Exercise: Loaders Optional Tasklist line 23 in package.json Add new src/client/assets/rainy.jpg src/client/index.js lines 3 to 7 adding for the image | line 9 to check the output src/client/views/index.html line 6 modify the path webpack.config.js lines 14 to 27 adding the file loader | comment line 7

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,10 @@
-import { checkForName } from './js/nameChecker'
-import { handleSubmit } from './js/formHandler'
-
+import { checkForName } from './js/nameChecker';
+import { handleSubmit } from './js/formHandler';
+import img from './assets/rainy.jpg';
+const imgContainer = document.getElementById('forImg');
+const image = document.createElement('img');
+image.src = img;
+imgContainer.appendChild(image);
 console.log(checkForName);
-
+console.log(handleSubmit);
 alert("I EXIST")
--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,27 @@ const webpack = require('webpack')
 module.exports = {
     entry: './src/client/index.js',
    // output: { ...output options },
-   mode: 'development',
+  // mode: 'development',
     module: {
         rules: [
                 {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: "babel-loader"
-                }
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: "babel-loader"
+                },
+
+                {
+                    test: /\.(png|jpe?g|gif)$/i,
+                    use: [
+                      {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            publicPath: './assets',
+                          },
+                      }
+                    ],
+                },
         ]
 }
 }
